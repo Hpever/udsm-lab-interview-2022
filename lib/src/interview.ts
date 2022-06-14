460de4a0b08b84efc43246639fa7f980ee7e789e
Hopever Kategile <hopeverkategile@gmail.com>
2022-06-14 11:47:59 +0300

    Implemented required logic to Calculate Average

--- lib/src/interview.ts
+++ lib/src/interview.ts
@@ -23,5 +23,34 @@ export function getProcessedData(
     ]
    */
 
-  return;
+  /* 1. Merge all data set into one and filter for reporting rate less than 50*/
+  const allYearsData = ([].concat(yearOneData, yearTwoData, yearThreeData))
+    .filter(data => data.reportingRate >= 50);
+
+  /* 2. Combining relevant data (data of the same code) from each year inorder to compute average*/
+  const combinedRelevantData: any = {
+    MEMBER_REGISTERED: { sumOfValues: 0, numOfValues: 0, name: '' },
+    ACTIVE_MEMBERS: { sumOfValues: 0, numOfValues: 0, name: '' }
+  };
+
+  allYearsData.forEach(data => {
+    const dataSet = combinedRelevantData[`${data.code}`];
+    dataSet.sumOfValues += data.value;
+    dataSet.numOfValues += 1;
+    dataSet.name = data.name;
+  })
+
+  /* 3. Computing average for the combined data to arrive to single value (average value) */
+
+  const computedAvarages: Array<any> = [];
+
+  Object.keys(combinedRelevantData).forEach(key => {
+    const dataSet = combinedRelevantData[key];
+    computedAvarages.push({
+      value: dataSet.sumOfValues / dataSet.numOfValues,
+      name: dataSet.name,
+      code: key
+    })
+  })
+  return computedAvarages;
 }
